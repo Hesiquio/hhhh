478282b80b500a7d9ba9ab99ff94ed461a730476
Hesiquio <zarate@itsescarcega.edu.mx>
2017-07-13 12:13:01 -0500

    2.0

--- calc.py
+++ calc.py
@@ -1,19 +1,26 @@
 # Este programa suma dos numeros
 print("Calculadora")
 
-sel=0
-while(sel!=6):
+sel=99
+while(sel!=0):
 	print(" ")
+	print("Menu de Opciones: ")
 	print("1 Suma")
 	print("2 Resta")
-	print("6 Salir")
-	sel=int(input("Ingrese el numero de la operacion que desea ejecutar: "))
+	print("3 Multiplicacion")
+	print("4 Division")
+	print("0 Salir")
+	print(" ")
+	
+	sel=int(input("Ingrese el numero de la opcion que desea ejecutar: "))
+	
 	if sel == 1:
 		print("Suma")
 		a=int(input("Ingrese el numero 1: "))
 		b=int(input("Ingrese el numero 2: "))
 		suma=a+b
 		print("El resultado de la suma es:", suma)
+		print(" ")
 
 	elif sel==2:
 		print("Resta")
@@ -21,7 +28,27 @@ while(sel!=6):
 		b=int(input("Ingrese el numero 2: "))
 		resta=a-b
 		print("El resultado de la resta es:", resta)
-	elif sel==6:
+		print(" ")
+		
+	elif sel==3:
+		print("Multiplicacion")
+		a=int(input("Ingrese el numero 1: "))
+		b=int(input("Ingrese el numero 2: "))
+		mult=a*b
+		print("El resultado de la multiplicacion es:", mult)
+		print(" ")
+		
+	elif sel==4:
+		print("Division")
+		a=int(input("Ingrese el numero 1: "))
+		b=int(input("Ingrese el numero 2: "))
+		div=a/b
+		print("El resultado de la divicion es:", div)
+		print(" ")
+		
+	elif sel==0:
 		print("Adios")
+		
 	else:
 		print("Opcion no valida")
+		
